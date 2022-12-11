e68ca2f12beaf484ccc3d4282c6ca67a07be656b
Jeff Morton <jmorton138@gmail.com>
2022-12-11 09:12:01 +0000

    [ADD] Adds new class for creating new pages

---
+++ LibraryManagerApp/src/NewPage.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.lang.Exception;
+
+public class NewPage extends JFrame {
+    JFrame frame;
+
+    NewPage() {
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        frame = new JFrame();
+
+        frame.setTitle("Welcome");
+        frame.setSize(700, 700);
+
+    }
+}
