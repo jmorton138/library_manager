8d549e84416e7d3738aa0f986061d72bd26f41d6
Jeff Morton <jmorton138@gmail.com>
2023-01-12 07:10:53 +0000

    [ADD] Connects to mysql database with name 'library_db'

---
+++ LibraryManagerApp/src/DatabaseConnection.java
@@ -0,0 +1,28 @@
+import java.awt.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.*;
+
+public class DatabaseConnection {
+    String name;
+    Connection connection;
+
+    DatabaseConnection(String name) throws Exception {
+        // gets the required driver
+        Class.forName("com.mysql.jdbc.Driver");
+        // creates connection with sql
+        this.name = name;
+        String db_username = "root";
+        String db_password = "password";
+        this.connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/" + name, db_username, db_password);
+        System.out.println(connection);
+    }
+
+    public Connection getConnection() {
+        System.out.print("get connection");
+        return this.connection;
+    }
+
+}
