063a2876865172f51bd66983b70e310741388807
Jeff Morton <jmorton138@gmail.com>
2023-01-28 07:50:12 +0000

    [IMP] 1.1: Creates Frame class extending JFrame for DRY frame instantiation

---
+++ LibraryManagerApp/src/Frame.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Frame extends JFrame {
+
+    Frame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500, 500);
+        this.setVisible(true);
+    }
+
+}
