61b9b4f97d05bfd25108488ece22953b1321e724
Jeff Morton <jmorton138@gmail.com>
2023-01-29 10:52:35 +0000

    [IMP] Adds borderlayout panels to newpage constructor

--- LibraryManagerApp/src/NewPage.java
+++ LibraryManagerApp/src/NewPage.java
@@ -4,14 +4,37 @@ import java.awt.event.*;
 import java.lang.Exception;
 
 public class NewPage extends JFrame {
-    JFrame frame;
+    JPanel centerPanel;
+    JPanel northPanel;
+    JPanel southPanel;
+    JPanel eastPanel;
+    JPanel westPanel;
 
     NewPage() {
-        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
-        frame = new JFrame();
+        this.setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        this.setSize(900, 900);
+        this.centerPanel = new JPanel();
+        this.centerPanel.setBackground(Color.orange);
+        this.northPanel = new JPanel();
+        this.northPanel.setBackground(Color.red);
+        this.southPanel = new JPanel();
+        this.southPanel.setBackground(Color.blue);
+        this.eastPanel = new JPanel();
+        this.eastPanel.setBackground(Color.green);
+        this.westPanel = new JPanel();
+        this.westPanel.setBackground(Color.magenta);
 
-        frame.setTitle("Welcome");
-        frame.setSize(700, 700);
+        this.northPanel.setPreferredSize(new Dimension(100, 100));
+        this.centerPanel.setPreferredSize(new Dimension(100, 100));
+        this.eastPanel.setPreferredSize(new Dimension(100, 100));
+        this.northPanel.setPreferredSize(new Dimension(100, 100));
+        this.southPanel.setPreferredSize(new Dimension(100, 100));
+        this.westPanel.setPreferredSize(new Dimension(100, 100));
+        this.add(northPanel, BorderLayout.NORTH);
+        this.add(southPanel, BorderLayout.SOUTH);
+        this.add(centerPanel, BorderLayout.CENTER);
+        this.add(eastPanel, BorderLayout.EAST);
+        this.add(westPanel, BorderLayout.WEST);
 
     }
 }
