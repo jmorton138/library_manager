4fe2b301da0803bf4ae83efcc5cf90dceab749d4
Jeff Morton <jmorton138@gmail.com>
2023-02-04 08:26:43 +0000

    [ADD] 1.2: Adds more navigation buttons to home page

--- LibraryManagerApp/src/HomePage.java
+++ LibraryManagerApp/src/HomePage.java
@@ -6,21 +6,53 @@ import java.sql.*;
 
 public class HomePage extends NewPage {
     Button categoryButton;
+    Button booksButton;
+    Button authorsButton;
+    Button issueBookButton;
+    Button returnBookButton;
+    JPanel navPanel;
     Connection conn;
 
     HomePage(Connection conn) {
         this.conn = conn;
+        navPanel = new JPanel();
+        navPanel.setLayout(null);
         categoryButton = new Button("Category");
-        categoryButton.setBounds(400, 200, 300, 40);
         categoryButton.addActionListener(categoryClick -> {
             ViewCategoriesPage viewCategoriesPage = new ViewCategoriesPage(conn);
             try {
                 viewCategoriesPage.viewCategories();
             } catch (SQLException throwables) {
-                String string;
+                System.out.println(throwables.getLocalizedMessage());
+
             }
         });
-        this.add(categoryButton);
+
+        booksButton = new Button("Books");
+        booksButton.addActionListener(booksClick -> {
+            ViewBooksPage viewBooksPage = new ViewBooksPage(this.conn);
+            try {
+                viewBooksPage.viewBooks();
+
+            } catch (SQLException throwables) {
+                System.out.println(throwables.getLocalizedMessage());
+            }
+        });
+        authorsButton = new Button("Authors");
+
+        issueBookButton = new Button("Issue Book");
+
+        returnBookButton = new Button("Return Book");
+
+        GridLayout layout = new GridLayout(0, 1);
+        layout.setVgap(10);
+        navPanel.setLayout(layout);
+        navPanel.add(categoryButton);
+        navPanel.add(booksButton);
+        navPanel.add(authorsButton);
+        navPanel.add(issueBookButton);
+        navPanel.add(returnBookButton);
+        this.centerPanel.add(navPanel);
 
     }
 }
